a11a9ee225145c14cacbb27a7ac489cb71bbf98f
Joe21-dev <joedsmith2550@gmail.com>
2025-07-23 16:58:16 +0300

    added

--- server/index.js
+++ server/index.js
@@ -32,7 +32,10 @@ cloudinary.config({
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
+ 
 dotenv.config();
+
+
 
 const app = express();
 
@@ -123,6 +126,7 @@ if (!MONGO_URI) {
   console.error('MONGO_URI is not defined in .env');
   process.exit(1);
 }
+console.log('Connecting to MongoDB at:', MONGO_URI);
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
